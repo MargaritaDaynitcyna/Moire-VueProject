37758fc7625729fa7a632b996918df7fad53d58e
Margarita <90200312+MargaritaDaynitcyna@users.noreply.github.com>
2023-06-26 19:07:13 +0700

    add Base Pagination

--- moire/src/App.vue
+++ moire/src/App.vue
@@ -2,68 +2,35 @@
     <section class="catalog">
         <ProductList :products="products"></ProductList>
 
-        <ul class="catalog__pagination pagination">
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow pagination__link--disabled" aria-label="Предыдущая страница">
-              <svg width="8" height="14" fill="currentColor">
-                <use xlink:href="#icon-arrow-left"></use>
-              </svg>
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--current">
-              1
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              2
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              3
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              4
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              ...
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link" href="#">
-              10
-            </a>
-          </li>
-          <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница">
-              <svg width="8" height="14" fill="currentColor">
-                <use xlink:href="#icon-arrow-right"></use>
-              </svg>
-            </a>
-          </li>
-        </ul>
+        <BasePagination :count="countProducts" v-model="page" :per-page="productsPerPage"></BasePagination>
+
       </section>
 </template>
 
 <script>
-import products from './data/products'
+import Allproducts from './data/products'
 import ProductList from './components/ProductList.vue'
+import BasePagination from './components/BasePagination.vue'
 
 export default {
-  components: { ProductList },
+  components: { ProductList, BasePagination },
   name: 'App',
   data() {
     return {
-      // products: products
-      products
+      // products: products = products,
+      page: 1,
+      productsPerPage: 3,
     }
-  }
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return Allproducts.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return Allproducts.length;
+    }
+  },
   
 }
 </script>
--- moire/src/components/ProductList.vue
+++ moire/src/components/ProductList.vue
@@ -1,8 +1,8 @@
 <template>
   <ul class="catalog__list">
-    <li class="catalog__item" v-for="(product, index) in products" :key="index">
-
-    <ProductItem :product="product"></ProductItem>
+    <li class="catalog__item" v-for="(product) in products" :key="product.title">
+      <!-- key сменить на id -->
+      <ProductItem :product="product"></ProductItem>
     </li>
   </ul>
 </template>
